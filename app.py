5511e939d09ec817b1d0a1944c280904d9d810f0
ElDavicillo <davidpzkw@gmail.com>
2025-05-05 17:27:03 -0600

    Corrijo ruta al archivo CSV

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
-import streamlit as st
 import pandas as pd
-import plotly_express as px
+import plotly.express as px
+import streamlit as st
 
-car_data = pd.read_csv(
-    r'C:\Users\david\OneDrive\Escritorio\TripleTen Scripts\my_project_sprint7\vehicles_us.csv')  # leer los datos
-hist_button = st.button('Construir histograma')  # crear un botón
+# Leer los datos una vez
+car_data = pd.read_csv("vehicles_us.csv")  # leer los datos
+
+# Botón para construir el histograma
+hist_button = st.button('Construir histograma')
 
 if hist_button:  # al hacer clic en el botón
-    # escribir un mensaje
+    # Escribir un mensaje
     st.write(
         'Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
 
-    # crear un histograma
+    # Crear un histograma
     fig = px.histogram(car_data, x="odometer")
 
-    # mostrar un gráfico Plotly interactivo
+    # Mostrar el gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+
+# Crear un gráfico de dispersión
+fig = px.scatter(car_data, x="odometer", y="price")
+fig.show()  # Crear gráfico de dispersión
